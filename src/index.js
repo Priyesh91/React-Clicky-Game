05dc44e05806ef298dbfc2787bf39fcdd553a05e
Priyesh <91priyesh@gmail.com>
2019-03-05 10:38:37 -0500

    copied over router from project 3, as a template to work with

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+const Root = () => (
+  <React.Fragment>
+    <Router>
+    <div>
+     
+    
+      <Switch>
+        <Route component={App} exact path="/" />
+         
+      </Switch>
+    </div>
+    </Router>
+  </React.Fragment>
+)
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
